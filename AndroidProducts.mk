de99d0675ffadcd35a1876e00e19fe6951580a21
youngherna <hernan.mauricio16@gmail.com>
2023-12-18 14:22:48 +0000

    Derpfest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_courbet.mk
+    $(LOCAL_DIR)/derp_courbet.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_courbet-user \
-    aosp_courbet-userdebug \
-    aosp_courbet-eng
+    derp_courbet-user \
+    derp_courbet-userdebug \
+    derp_courbet-eng
